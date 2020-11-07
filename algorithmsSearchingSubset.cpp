80a5599f86b311c6ab2afa445eb837c4570b76e5
kirrealdev <33735890+kirrealdev@users.noreply.github.com>
2020-11-07 20:04:17 +0300

    Add files via upload
    add second realisation rkSearch

--- algorithmsSearchingSubset.cpp
+++ algorithmsSearchingSubset.cpp
@@ -2,31 +2,28 @@
 #include "iostream"
 using namespace std;
 #define BASE 10000000
-int naiveSearch(string firstString, string secondString) {
+
+int naiveSearch(string text, string str) {
 	
-    if(firstString == "" or secondString == "") return -1;
+    if(text == "" || str == "") return -1;
     
-    int lengthFirstStr = firstString.length();
-	int lengthSecondStr = secondString.length();
-	int numberOfMatchedChars = 0;
+    unsigned int textLength = text.length();
+	unsigned int strLength = str.length();
+	unsigned int numberOfMatches = 0;
 	
-    for (int i = 0; i <= lengthFirstStr - lengthSecondStr; i++) {
-		for (int j = i; j < i + lengthSecondStr; j++) {
-			if (firstString[j] == secondString[j - i]) {
-				numberOfMatchedChars++;
-                if (numberOfMatchedChars == lengthSecondStr)
-                    return i;
-            }
-			else 
-				numberOfMatchedChars = 0;
-		}
+    for (unsigned int i = 0; i <= (textLength - strLength); i++) {
+        for (unsigned int j = i; j < (i + strLength) && text[j] == str[j - i]; j++) {
+            numberOfMatches++;
+            if (numberOfMatches == strLength) return i;
+        }
+	    numberOfMatches = 0;
 	}
 	return -1;
 }
 
 int KMP(string t, string p) {
 	
-    if(t == "" or p == "") return -1;
+    if(t == "" || p == "") return -1;
     
     int n = t.length();
 	int m = p.length();
@@ -55,9 +52,9 @@ int KMP(string t, string p) {
 	return -1;
 }
 
-int rabinKarpSearch(string source, string target){
+int rabinKarpSearch2(string source, string target){
         
-        if(source == "" or target == "") return -1;
+        if(source == "" || target == "") return -1;
         int m = target.size();
         int power = 1;
         for(int i = 0;i<m;i++){
@@ -84,12 +81,72 @@ int rabinKarpSearch(string source, string target){
         return -1;
     }
 
+int hInit(unsigned int strLength) {
+    
+    int d = 52;
+    int p = 65713;
+
+    int h = 1;
+    for(unsigned int i = 1; i < strLength; i++) {
+        h = (h * d) % p;
+    }
+
+    return h;
+}
+
+int ringHash(string str, unsigned int strLength, int prevHash, int *h) {
+    
+      // h = d^(len-1) (mod p) - константа для быстроого пересчета хэша
+    int d = 52; // Константа из формулы
+    int p = 65713; // Вычисления производятся по модулю простого числа
+    
+    if(*h == 0) *h = hInit(strLength);
+
+    if(prevHash == 0) {
+        for(unsigned int i = 0; i < strLength; i++) {
+            prevHash += (d*prevHash + (int)str[i]) % p;
+        }
+        if(prevHash < 0) prevHash += p;
+        return prevHash;
+    }
+    else {
+        int hash = (d * (prevHash - (int)str[0] * (*h)) + (int)str[strLength]) % p;
+        if(hash < 0) hash += p;
+        return hash;
+    }
+}
+	int rabinKarpSearch(string text, string str) {
+        
+		if(text == "" or str == "") return -1;
+
+		unsigned int textLength = text.length();
+		unsigned int strLength = str.length();
+        
+		int h = 0;
+		
+        //Хэш подстроки для поиска
+        int strHash = ringHash(str, strLength, 0, &h);
+        //Хэш первого окна в тексте
+        int textHash = ringHash(text, strLength, 0, &h);
+
+        for(unsigned int i = 0; i <= (textLength-strLength); i++) {
+            if(strHash == textHash) {
+                for(unsigned int j = 0; (j < strLength) && (str[j] == text[i+i]); j++) {
+                    if(j == (strLength - 1)) return i;
+                }
+            }
+            textHash = ringHash(&text[i], strLength, textHash, &h);
+        }
+        return -1;
+}
+
 int main()
 {
-	string firstStr = "ffjfjsadjdkfsaffjkfsa";
-	string secondStr = "sa";
+	string firstStr = "cbaaaaacb";
+	string secondStr = "cb";
 	cout <<"naive = "<<naiveSearch(firstStr, secondStr)<<"\n";
 	cout <<"KMP = "<< KMP(firstStr, secondStr)<<"\n";
 	cout << "RK = " << rabinKarpSearch(firstStr, secondStr) << "\n";
+	cout << "RK = " << rabinKarpSearch2(firstStr, secondStr) << "\n";
 	return 0;
 }
