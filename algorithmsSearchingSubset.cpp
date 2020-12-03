3b6c4e34d2ecd4d8961740b92ed708f57c7c5963
kirrealdev <33735890+kirrealdev@users.noreply.github.com>
2020-12-04 01:29:32 +0300

    Add files via upload
    bug fix

--- algorithmsSearchingSubset.cpp
+++ algorithmsSearchingSubset.cpp
@@ -1,7 +1,7 @@
 #include  <vector>
 #include "iostream"
 using namespace std;
-#define BASE 10000000
+#include <math.h>
 
 int naiveSearch(string text, string str) {
 	
@@ -12,7 +12,7 @@ int naiveSearch(string text, string str) {
 	unsigned int numberOfMatches = 0;
 	
     for (unsigned int i = 0; i <= (textLength - strLength); i++) {
-        for (unsigned int j = i; j < (i + strLength) && text[j] == str[j - i]; j++) {
+        for (unsigned int j = i; j < (i + strLength) && (text[j] == str[j - i]); j++) {
             numberOfMatches++;
             if (numberOfMatches == strLength) return i;
         }
@@ -21,132 +21,87 @@ int naiveSearch(string text, string str) {
 	return -1;
 }
 
-int KMP(string t, string p) {
-	
-    if(t == "" || p == "") return -1;
-    
-    int n = t.length();
-	int m = p.length();
-
-	int *d = new int[m];
-	d[0] = 0;
-	for (int i = 1, j = 0; i < m; i++) {
-		while (j > 0 && p[j] != p[i])
-			j = d[j - 1];
-		if (p[j] == p[i])
-			j++;
-		d[i] = j;	
-	}
-	for (int i = 0, j = 0; i < n; i++) {
-		while (j > 0 && p[j] != t[i])
-			j = d[j - 1];
-		if (p[j] == t[i])
-			j++;
-		if (j == m)
-		{
-			delete d;
-			return i - j + 1;
-		}
-	}
-	delete d;
-	return -1;
-}
-
-int rabinKarpSearch2(string source, string target){
-        
-        if(source == "" || target == "") return -1;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
-        }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
-            }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
-            }
-        }
-        return -1;
-    }
-
-int hInit(unsigned int strLength) {
-    
-    int d = 52;
-    int p = 65713;
-
-    int h = 1;
-    for(unsigned int i = 1; i < strLength; i++) {
-        h = (h * d) % p;
-    }
-
-    return h;
-}
-
-int ringHash(string str, unsigned int strLength, int prevHash, int *h) {
-    
-      // h = d^(len-1) (mod p) - константа для быстроого пересчета хэша
-    int d = 52; // Константа из формулы
-    int p = 65713; // Вычисления производятся по модулю простого числа
-    
-    if(*h == 0) *h = hInit(strLength);
-
-    if(prevHash == 0) {
-        for(unsigned int i = 0; i < strLength; i++) {
-            prevHash += (d*prevHash + (int)str[i]) % p;
-        }
-        if(prevHash < 0) prevHash += p;
-        return prevHash;
-    }
-    else {
-        int hash = (d * (prevHash - (int)str[0] * (*h)) + (int)str[strLength]) % p;
-        if(hash < 0) hash += p;
-        return hash;
+void prefixFuncForKMP(const string &str, vector<int> &pi) {
+ 
+    pi.resize(str.size());
+    pi[0] = 0;
+  
+    for (int i = 1; i < pi.size(); ++i) {
+        int pos = pi[i - 1];
+        while (pos > 0 && str[pos] != str[i])
+            pos = pi[pos - 1];
+        if(str[pos] == str[i]) pi[i] = pos + 1;
+        else pi[i] = pos;
     }
 }
-	int rabinKarpSearch(string text, string str) {
-        
-		if(text == "" or str == "") return -1;
 
-		unsigned int textLength = text.length();
-		unsigned int strLength = str.length();
-        
-		int h = 0;
-		
-        //Хэш подстроки для поиска
-        int strHash = ringHash(str, strLength, 0, &h);
-        //Хэш первого окна в тексте
-        int textHash = ringHash(text, strLength, 0, &h);
+int searchKMP(string text, string str) {
+  
+    if(text == "" || str == "") return -1;
+  
+    unsigned int textLength = text.length();
+    unsigned int strLength = str.length();
+    vector<int> pi;
+    prefixFuncForKMP(str, pi);
+    int pos = 0;
+  
+    for (int i = 0; i < textLength; ++i) {
+        while (pos > 0 && (pos >= strLength || str[pos] != text[i]))
+            pos = pi[pos-1];
+        if (text[i] == str[pos]) pos++;
+        if (pos == str.size()) return (i - pos + 1);
+    }
+    
+    return -1;
+}
 
-        for(unsigned int i = 0; i <= (textLength-strLength); i++) {
-            if(strHash == textHash) {
-                for(unsigned int j = 0; (j < strLength) && (str[j] == text[i+i]); j++) {
-                    if(j == (strLength - 1)) return i;
+
+int searchRK(string text, string str) {
+    
+    unsigned int textLength = text.length();
+	unsigned int strLength = str.length();
+    int hashStr = 0;
+    int hashText = 0;
+    int count = 0;
+
+    for(int i = 0; i < strLength; i++) {
+        hashStr = hashStr + str[i] * pow(13, i);
+    }
+
+    for(int i = 0; i < strLength; i++) {
+        hashText = hashText + text[i] * pow(13, i);
+    }
+    count = 0;
+    for(int j = 0; j < textLength - strLength + 1; j++) {
+        
+        if(hashText == hashStr) {
+            for(int i = 0; i < strLength; i++) {
+                if(str[i] == text[j + i]) {
+                    count++;
                 }
             }
-            textHash = ringHash(&text[i], strLength, textHash, &h);
-        }
-        return -1;
+            if(count == strLength) {
+                return j;
+            }
+            else {
+                count = -1;
+            }  
+        } 
+        hashText = (hashText - text[j] * 1) / 13 + text[j + strLength] * pow(13, strLength - 1);
+    }
+    return -1;
 }
 
 int main()
 {
-	string firstStr = "cbaaaaacb";
+	string firstStr = "xbaaaaacb";
 	string secondStr = "cb";
-	cout <<"naive = "<<naiveSearch(firstStr, secondStr)<<"\n";
-	cout <<"KMP = "<< KMP(firstStr, secondStr)<<"\n";
-	cout << "RK = " << rabinKarpSearch(firstStr, secondStr) << "\n";
-	cout << "RK = " << rabinKarpSearch2(firstStr, secondStr) << "\n";
-	return 0;
+	
+    cout <<"naive search = "<<naiveSearch(firstStr, secondStr)<<"\n";
+    
+    cout <<"search KMP = "<< searchKMP(firstStr, secondStr)<<"\n";
+    
+    cout << "search RK = " << searchRK(firstStr, secondStr) << "\n";
+
+    return 0;
 }
