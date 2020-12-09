bfcc24b63a6b80cb0064e2ad082bdb6a61e7d1be
kirrealdev <33735890+kirrealdev@users.noreply.github.com>
2020-12-10 00:33:23 +0300

    bug fix

--- algorithmsSearchingSubset.cpp
+++ algorithmsSearchingSubset.cpp
@@ -2,6 +2,7 @@
 #include "iostream"
 using namespace std;
 #include <math.h>
+#include <time.h>
 
 int naiveSearch(string text, string str) {
 	
@@ -55,56 +56,99 @@ int searchKMP(string text, string str) {
     return -1;
 }
 
+int mod(int a, int b) {
+	
+	int ret = a%b; 
+	return ret>=0? ret: ret+b; 
+}
 
-int searchRK(string text, string str) {
-    
-    if(text == "" || str == "") return -1;
-    
-    int simpleNumber = 13;
-    unsigned int textLength = text.length();
-	unsigned int strLength = str.length();
-    int hashStr = 0;
-    int hashText = 0;
-    int count = 0;
+int new_RK(string T, string P) {
+	
+    int n = T.length();
+	int m = P.length();
+	int* H = new int[n];
+	int* A = new int[n];
+	int* anti_A = new int[n];
+	int difference = 0;
+	const int p = 131;
+	int count = 0;
+	int a = 127;
+    long long powerOfA = 1;
+	int curr_hash = 0;
+	
+	for (int i = 0; i < n; i++) {
+		A[i] = powerOfA;
+        powerOfA = mod((powerOfA * a), p);
+	}
+	
+	for (int i = 0; i < n; i++) {
+		int x = 0;
+		while (((mod((A[i] * x), p)) != 1) && (x < p)) {
+			x++;
+		}
+		anti_A[i] = x;
+	}
 
-    for(int i = 0; i < strLength; i++) {
-        hashStr = hashStr + str[i] * pow(simpleNumber, i);
-    }
+	H[0] = mod((T[0] * 1), p);
 
-    for(int i = 0; i < strLength; i++) {
-        hashText = hashText + text[i] * pow(simpleNumber, i);
-    }
-    count = 0;
-    for(int j = 0; j < textLength - strLength + 1; j++) {
-        
-        if(hashText == hashStr) {
-            for(int i = 0; i < strLength; i++) {
-                if(str[i] == text[j + i]) {
-                    count++;
-                }
-            }
-            if(count == strLength) {
-                return j;
-            }
-            else {
-                count = -1;
-            }  
-        } 
-        hashText = (hashText - text[j] * 1) / simpleNumber + text[j + strLength] * pow(simpleNumber, strLength - 1);
-    }
-    return -1;
+	for (int i = 1; i < n; i++) {
+		H[i] = mod((H[i - 1] + int(T[i]*A[i])), p);
+		
+	}
+	
+	int P_hash = 0;
+	for (int i = 0; i < m; i++) {
+		P_hash = mod((P_hash + (P[i] * A[i])), p);
+	}
+	
+	curr_hash = mod(H[m - 1], p);
+
+	if (curr_hash == P_hash) {
+		for (int k = 0; k < m; k++) {
+			if (P[k] == T[k]) 
+				count++;
+			}
+		if (count == m)
+			return 0;
+		else
+			count = -1; 
+	}
+
+	for (int j = m + 1; j < n; j++) {
+		count = 0;
+		int i = j - m;
+	
+		curr_hash = mod((H[j] - H[i])* anti_A[i + 1], p);
+		
+		if (curr_hash == P_hash) {
+			for (int k = 0; k < m; k++) {
+				if (P[k] == T[k + i + 1]) 
+					count++;
+			}
+			if (count == m)
+				return i + 1;
+			else
+				count = -1;
+		}
+	}
+	return -1;
 }
 
 int main()
 {
-	string firstStr = "xbaaaaacb";
-	string secondStr = "cb";
+	string firstStr = "caaaasdfgcqwerty123";
+	string secondStr = "qwerty123";
+	
+	clock_t start = clock();
 	
     cout <<"naive search = "<<naiveSearch(firstStr, secondStr)<<"\n";
-    
-    cout <<"search KMP = "<< searchKMP(firstStr, secondStr)<<"\n";
-    
-    cout << "search RK = " << searchRK(firstStr, secondStr) << "\n";
+	// cout <<"KMP search = "<< searchKMP(firstStr, secondStr)<<"\n";
+	// cout << "RK search = " << new_RK(firstStr, secondStr) << "\n";
+	clock_t finish = clock();
+
+	double timeInSec = ((double)(finish - start) / (double)CLOCKS_PER_SEC);
+    cout << "Time: " << timeInSec << " sec" << endl;
+ 
 
     return 0;
 }
