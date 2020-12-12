f567e53d4a4229a148ba6eba4177f095c33023dc
kirrealdev <33735890+kirrealdev@users.noreply.github.com>
2020-12-12 19:59:49 +0300

    small bug fix 1.1

--- algorithmsSearchingSubset.cpp
+++ algorithmsSearchingSubset.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <math.h>
 #include <chrono>
+#include <fstream>
 using namespace std;
 using namespace chrono;
 
@@ -34,7 +35,7 @@ resultOfAlgorithm naiveSearch(string text, string str) {
             numberOfMatches++;
             if (numberOfMatches == strLength) { 
 				auto end = system_clock::now();
-				duration<double> sec = end - start;
+				duration<double, milli> sec = end - start;
 				res.answer = i;
 				res.time = sec.count();
 				return res;
@@ -46,23 +47,19 @@ resultOfAlgorithm naiveSearch(string text, string str) {
 	return res;
 }
 
-vector<int> prefix_function(const string& s) {
+vector<int> prefixFunction(const string& s) {
     vector<int> pi(s.length(), 0);
 
     for (int i = 1; i < s.length(); i++) {
-        int j = pi[i - 1];  //текущая длина префикса, который мы хотим продолжить
-                            //гарантируется, что s[0..j-1] = s[i-j..i-1].
-
-        while (j > 0 && s[i] != s[j]) {     //пока мы не можем продолжить текущий префикс
-            j = pi[j - 1];  //уменьшаем его длину до следующей возможной
+        int j = pi[i - 1];  
+                            
+        while (j > 0 && s[i] != s[j]) {     
+            j = pi[j - 1];  
         }
-
-        //Теперь j - максимальная длина префикса, который мы можем продолжить,
-        //или 0, если такового не существует.
 
         if (s[i] == s[j]) {
             pi[i] = j + 1;
-        } else {    //такое может произойти только при j = 0
+        } else {   
             pi[i] = j;
         }
     }
@@ -70,22 +67,23 @@ vector<int> prefix_function(const string& s) {
     return pi;
 }
 
-resultOfAlgorithm kmp(string s, string t) {
+resultOfAlgorithm kmp(string text, string str) {
 	
 	resultOfAlgorithm res;
 	res.answer = -1;
 	res.time = -1;
+	if(text == "" || str == "") return res;
 
-	vector<int> pi = prefix_function(t + '#' + s);
+	vector<int> pi = prefixFunction(str + '#' + text);
 
-    int t_len = t.length();
+    int strLen = str.length();
 	auto start = system_clock::now();
     
-	for (int i = 0; i < s.length(); i++) {
-        if (pi[t_len + 1 + i] == t_len) {
+	for (int i = 0; i < text.length(); i++) {
+        if (pi[strLen + 1 + i] == strLen) {
         	auto end = system_clock::now();
-			duration<double> sec = end - start;
-			res.answer = (i - t_len + 1);
+			duration<double, milli> sec = end - start;
+			res.answer = (i - strLen + 1);
 			res.time = sec.count();
 			return res;
         }
@@ -100,14 +98,16 @@ int mod(int a, int b) {
 	return ret>=0? ret: ret+b; 
 }
 
-resultOfAlgorithm rk(string T, string P) {
+resultOfAlgorithm rk(string text, string str) {
 	
     resultOfAlgorithm res;
 	res.answer = -1;
 	res.time = -1;
 
-	int n = T.length();
-	int m = P.length();
+	if(text == "" || str == "") return res;
+
+	int n = text.length();
+	int m = str.length();
 	int* H = new int[n];
 	int* A = new int[n];
 	int* anti_A = new int[n];
@@ -131,16 +131,16 @@ resultOfAlgorithm rk(string T, string P) {
 		anti_A[i] = x;
 	}
 
-	H[0] = mod((T[0] * 1), p);
+	H[0] = mod((text[0] * 1), p);
 
 	for (int i = 1; i < n; i++) {
-		H[i] = mod((H[i - 1] + int(T[i]*A[i])), p);
+		H[i] = mod((H[i - 1] + int(text[i]*A[i])), p);
 		
 	}
 	
 	int P_hash = 0;
 	for (int i = 0; i < m; i++) {
-		P_hash = mod((P_hash + (P[i] * A[i])), p);
+		P_hash = mod((P_hash + (str[i] * A[i])), p);
 	}
 	
 	auto start = system_clock::now();
@@ -148,12 +148,12 @@ resultOfAlgorithm rk(string T, string P) {
 	curr_hash = mod(H[m - 1], p);
 	if (curr_hash == P_hash) {
 		for (int k = 0; k < m; k++) {
-			if (P[k] == T[k]) 
+			if (str[k] == text[k]) 
 				count++;
 			}
 		if (count == m) {
 			auto end = system_clock::now();
-			duration<double> sec = end - start;
+			duration<double, milli> sec = end - start;
 			res.answer = 0;
 			res.time = sec.count();
 			return res;
@@ -162,7 +162,7 @@ resultOfAlgorithm rk(string T, string P) {
 			count = -1; 
 	}
 
-	for (int j = m + 1; j < n; j++) {
+	for (int j = m; j < n; j++) {
 		count = 0;
 		int i = j - m;
 	
@@ -170,12 +170,12 @@ resultOfAlgorithm rk(string T, string P) {
 		
 		if (curr_hash == P_hash) {
 			for (int k = 0; k < m; k++) {
-				if (P[k] == T[k + i + 1]) 
+				if (str[k] == text[k + i + 1]) 
 					count++;
 			}
 			if (count == m) {
 				auto end = system_clock::now();
-				duration<double> sec = end - start;
+				duration<double, milli> sec = end - start;
 				res.answer = (i + 1);
 				res.time = sec.count();
 				return res;
@@ -189,75 +189,157 @@ resultOfAlgorithm rk(string T, string P) {
 }
 
 int main()
-{		
-    // cout <<"naive search = "<<naiveSearch(firstStr, secondStr)<<"\n";
-	// cout <<"KMP search = "<< KMPg(secondStr, firstStr)<<"\n";
-	// cout << "RK search = " << new_RK(firstStr, secondStr) << "\n";
-
+{	
 	string* FirstString = new string[10];
-	string* SecondString = new string[10];
-	FirstString[0] = "Я помню чудное мгновенье: передо мной явилась ты, как мимолетное виденье, как гений чистой красоты.";
-	SecondString[0] = "виденье";
-	FirstString[1] = "a^2 + b^2 = c^2";
-	SecondString[1] = "b";
-	FirstString[2] = "hallelujah";
-	SecondString[2] = "h";
-	FirstString[3] = "Real Madrid";
-	SecondString[3] = "rid";
-	FirstString[4] = "Всё зависит от нас самих, ничего в мире нет такого, что не подвластно было б нам.";
-	SecondString[4] = "н";
-	FirstString[5] = "Анализ и разработка алгоритмов";
-	SecondString[5] = "разработка";
-	FirstString[6] = "c++, python";
-	SecondString[6] = "t";
-	FirstString[7] = "ITMM";
-	SecondString[7] = "TM";
-	FirstString[8] = "Нижегородский государственный университет им. Н. И. Лобачевского";
-	SecondString[8] = "им.";
-	FirstString[9] = "One, two, three, four, five, six, seven, eight, nine, ten";
-	SecondString[9] = "five";
+	string* SecondStringTest1 = new string[10];
+	string* SecondStringTest2 = new string[10];
 	
-	resultOfAlgorithm* naiveResults = new resultOfAlgorithm[10];
-	dataForPlot* naiveSearchData = new dataForPlot[10];
+	FirstString[0] = "abcdefghijklmnop"; //16
+	SecondStringTest1[0] = "mnop";
+	SecondStringTest2[0] = "fghi";
+	
+	FirstString[1] = "Real Madrid Footbal Club"; //24
+	SecondStringTest1[1] = "l Club";
+	SecondStringTest2[1] = "Madrid";
+	
+	FirstString[2] = "Анализ и разработка алгоритмов!!"; //32
+	SecondStringTest1[2] = "ритмов!!";
+	SecondStringTest2[2] = "разработ";
+	
+	FirstString[3] = "U got mud on your face, you big disgrace"; //40
+	SecondStringTest1[3] = "disgrace";//8
+	SecondStringTest2[3] = " your face";//10
+	
+	FirstString[4] = "Каждый охотник желает знать, где сидит фазан"; //44
+	SecondStringTest1[4] = "сидит фазан";//11
+	SecondStringTest2[4] = "желает знать";//12
+	
+	FirstString[5] = "Институт Информационных Технологий Математики и Механики"; //56
+	SecondStringTest1[5] = "ики и Механики";
+	SecondStringTest2[5] = "Информационных";
+	
+	FirstString[6] = "Нижегородский государственный университет им. Н. И. Лобачевского";//64
+	SecondStringTest1[6] = " И. Лобачевского";
+	SecondStringTest2[6] = "государственный ";
+	
+	FirstString[7] = "One, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve"; //73
+	SecondStringTest1[7] = "ten, eleven, twelve";
+	SecondStringTest2[7] = " six, seven, eight";
+	
+	FirstString[8] = "Всё зависит от нас самих, ничего в мире нет такого, что не подвластно было б нам"; //80
+	SecondStringTest1[8] = "одвластно было б нам";
+	SecondStringTest2[8] = "чего в мире нет тако";
+	
+	FirstString[9] = "Я помню чудное мгновенье: передо мной явилась ты, как мимолетное виденье, как гений чистой красоты."; // 99
+	SecondStringTest1[9] = "как гений чистой красоты.";
+	SecondStringTest2[9] = "ты, как мимолетное виденье";
+	
+	ofstream fout;
+  	fout.open("results.txt");
+	fout << "+++++++++++++++++++++++++++++++PART A+++++++++++++++++++++++++++++++++++" << endl;
+	fout << "NAIVE ALGORIHTM:" << endl;
 	for (int i = 0; i < 10; i++) {
-		naiveResults[i] = naiveSearch(FirstString[i], SecondString[i]);
-		// cout << "naive answer = " << naiveResults[i].answer << "\n";
-		// cout << "naive time = " << naiveResults[i].time << "\n";
+		fout << "<< test #" << i << " >>" << endl;
 		
-		naiveSearchData[i].length = FirstString[i].length() + SecondString[i].length();
-		naiveSearchData[i].time = naiveResults[i].time;
-		cout << "naive data length = " << naiveSearchData[i].length << "\n";
-		cout << "naive data time = " << naiveSearchData[i].time << "\n";
-		cout << "-----"  << "\n";
+		fout << "end: " << "(" << FirstString[i].length() + SecondStringTest1[i].length() << ", " << naiveSearch(FirstString[i], SecondStringTest1[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString[i].length() + SecondStringTest2[i].length() << ", " << naiveSearch(FirstString[i], SecondStringTest2[i]).time << ")" << endl;
 	}
-	cout << "------------------------------------------"  << "\n";
-	resultOfAlgorithm* rkResults = new resultOfAlgorithm[10];
-	dataForPlot* rkSearchData = new dataForPlot[10];
+	
+	fout << "--------------------------------------------------------------------------------" << endl;
+	fout << "RK ALGORIHTM:" << endl;
+	
 	for (int i = 0; i < 10; i++) {
-		rkResults[i] = rk(FirstString[i], SecondString[i]);
-		// cout << "rk answer = " << rkResults[i].answer << "\n";
-		// cout << "rk time = " << rkResults[i].time << "\n";
+		fout << "<< test #" << i << " >>" << endl;
 		
-		rkSearchData[i].length = FirstString[i].length() + SecondString[i].length();
-		rkSearchData[i].time = rkResults[i].time;
-		cout << "rk data length = " << rkSearchData[i].length << "\n";
-		cout << "rk data time = " << rkSearchData[i].time << "\n";
-		cout << "-----"  << "\n";
+		fout << "end: " << "(" << FirstString[i].length() + SecondStringTest1[i].length() << ", " << rk(FirstString[i], SecondStringTest1[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString[i].length() + SecondStringTest2[i].length() << ", " << rk(FirstString[i], SecondStringTest2[i]).time << ")" << endl;
 	}
-	cout << "------------------------------------------"  << "\n";
-	resultOfAlgorithm* kmpResults = new resultOfAlgorithm[10];
-	dataForPlot* kmpSearchData = new dataForPlot[10];
+	
+	fout << "--------------------------------------------------------------------------------" << endl;
+	fout << "KMP ALGORIHTM:" << endl;
+	
 	for (int i = 0; i < 10; i++) {
-		kmpResults[i] = kmp(FirstString[i], SecondString[i]);
-		// cout << "kmp answer = " << kmpResults[i].answer << "\n";
-		// cout << "kmp time = " << kmpResults[i].time << "\n";
+		fout << "<< test #" << i << " >>" << endl;
 		
-		kmpSearchData[i].length = FirstString[i].length() + SecondString[i].length();
-		kmpSearchData[i].time = kmpResults[i].time;
-		cout << "kmp data length = " << kmpSearchData[i].length << "\n";
-		cout << "kmp data time = " << kmpSearchData[i].time << "\n";
-		cout << "-----"  << "\n";
+		fout << "end: " << "(" << FirstString[i].length() + SecondStringTest1[i].length() << ", " << kmp(FirstString[i], SecondStringTest1[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString[i].length() + SecondStringTest2[i].length() << ", " << kmp(FirstString[i], SecondStringTest2[i]).time << ")" << endl;
 	}
 
-    return 0;
+	fout << "+++++++++++++++++++++++++++++++PART B+++++++++++++++++++++++++++++++++++" << endl;
+	string* FirstString2 = new string[10];
+	string* SecondStringTest4 = new string[10];
+	string* SecondStringTest5 = new string[10];
+	
+	FirstString2[0] = "fskjfjsdkfjsdjkf"; //16
+	SecondStringTest4[0] = "djkf";
+	SecondStringTest5[0] = "sdkf";
+	
+	FirstString2[1] = "fskjfjsdkfjsdjkfweiowdmb"; //24
+	SecondStringTest4[1] = "iowdmb";
+	SecondStringTest5[1] = "dkfjsd";
+	
+	FirstString2[2] = "fskjfjsdkfjsdjkfweiowdmboppaqaax"; //32
+	SecondStringTest4[2] = "oppaqaax";
+	SecondStringTest5[2] = "kfjsdjkf";
+	
+	FirstString2[3] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvv"; //40
+	SecondStringTest4[3] = "axzzxxccvv";
+	SecondStringTest5[3] = "jsdjkfweio";
+	
+	FirstString2[4] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighik"; //48
+	SecondStringTest4[4] = "ccvvyuyighik";
+	SecondStringTest5[4] = "weiowdmboppa";
+	
+	FirstString2[5] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighikalllolop"; //56
+	SecondStringTest4[5] = "yighikalllolop";
+	SecondStringTest5[5] = "iowdmboppaqaax";
+	
+	FirstString2[6] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighikalllolophgfdjkfs";//64
+	SecondStringTest4[6] = "alllolophgfdjkfs";
+	SecondStringTest5[6] = "boppaqaaxzzxxccv";
+	
+	FirstString2[7] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighikalllolophgfdjkfsnmbvcdde"; //72
+	SecondStringTest4[7] = "ophgfdjkfsnmbvcdde";
+	SecondStringTest5[7] = "dmboppaqaaxzzxxccv";
+	
+	FirstString2[8] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighikalllolophgfdjkfsnmbvcddecnjaskas"; //80
+	SecondStringTest4[8] = "jkfsnmbvcddecnjaskas";
+	SecondStringTest5[8] = "ccvvyuyighikalllolop";
+	
+	FirstString2[9] = "fskjfjsdkfjsdjkfweiowdmboppaqaaxzzxxccvvyuyighikalllolophgfdjkfsnmbvcddecnjaskasxcasdfsd"; // 88
+	SecondStringTest4[9] = "bvcddecnjaskasxcasdfsd";
+	SecondStringTest5[9] = "axzzxxccvvyuyighikalll";
+  	
+	fout << "NAIVE ALGORIHTM:" << endl;
+	for (int i = 0; i < 10; i++) {
+		fout << "<< test #" << i << " >>" << endl;
+		
+		fout << "end: " << "(" << FirstString2[i].length() + SecondStringTest4[i].length() << ", " << naiveSearch(FirstString2[i], SecondStringTest4[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString2[i].length() + SecondStringTest5[i].length() << ", " << naiveSearch(FirstString2[i], SecondStringTest5[i]).time << ")" << endl;
+	}
+	
+	fout << "--------------------------------------------------------------------------------" << endl;
+	fout << "RK ALGORIHTM:" << endl;
+
+	for (int i = 0; i < 10; i++) {
+		fout << "<< test #" << i << " >>" << endl;
+		
+		fout << "end: " << "(" << FirstString2[i].length() + SecondStringTest4[i].length() << ", " << rk(FirstString2[i], SecondStringTest4[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString2[i].length() + SecondStringTest5[i].length() << ", " << rk(FirstString2[i], SecondStringTest5[i]).time << ")" << endl;
+	}
+	
+	fout << "--------------------------------------------------------------------------------" << endl;
+	fout << "KMP ALGORIHTM:" << endl;
+
+
+	for (int i = 0; i < 10; i++) {
+		fout << "<< test #" << i << " >>" << endl;
+		
+		fout << "end: " << "(" << FirstString2[i].length() + SecondStringTest4[i].length() << ", " << kmp(FirstString2[i], SecondStringTest4[i]).time << ")" << endl;
+		fout << "middle: " << "(" << FirstString2[i].length() + SecondStringTest5[i].length() << ", " << kmp(FirstString2[i], SecondStringTest5[i]).time << ")" << endl;
+	}
+
+	fout.close();
+    
+	return 0;
 }
